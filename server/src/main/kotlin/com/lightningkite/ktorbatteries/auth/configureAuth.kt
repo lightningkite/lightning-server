e3ef4d4e31593f55a2983fa5e000ac611190b90a
Joseph Ivie <joseph@lightningkite.com>
2022-05-31 13:27:30 -0600

    Added get self endpoint

--- server/src/main/kotlin/com/lightningkite/ktorbatteries/auth/configureAuth.kt
+++ server/src/main/kotlin/com/lightningkite/ktorbatteries/auth/configureAuth.kt
@@ -84,6 +84,13 @@ inline fun <reified USER : HasId<ID>, reified ID : Comparable<ID>> Application.c
             oauthGoogle() { userByEmail(it)._id.toString() }
             oauthGithub() { userByEmail(it)._id.toString() }
             oauthApple() { userByEmail(it)._id.toString() }
+            get(
+                path = "self",
+                summary = "Get Self",
+                description = "Retrieves the user that you currently are",
+                errorCases = listOf(),
+                implementation = { user: USER, _: Unit -> user }
+            )
         }
     }
 }
