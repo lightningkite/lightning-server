a2516960fa9f8244f346fb6301ed8402ce98282d
Brady Svedin <brady@lightningkite.com>
2022-11-01 17:45:38 -0600

    Moved s3 HEAD option to second rule.

--- server-aws/src/main/kotlin/com/lightningkite/lightningserver/aws/terraform.kt
+++ server-aws/src/main/kotlin/com/lightningkite/lightningserver/aws/terraform.kt
@@ -165,14 +165,14 @@ internal fun handlers() {
                 
                   cors_rule {
                     allowed_headers = ["*"]
-                    allowed_methods = ["PUT", "POST", "HEAD"]
+                    allowed_methods = ["PUT", "POST"]
                     allowed_origins = ["*"]
                     expose_headers  = ["ETag"]
                     max_age_seconds = 3000
                   }
                 
                   cors_rule {
-                    allowed_methods = ["GET"]
+                    allowed_methods = ["GET", "HEAD"]
                     allowed_origins = ["*"]
                   }
                 }
