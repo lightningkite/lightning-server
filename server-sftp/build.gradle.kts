7ff58704674f9643696956d722edb495ced737ee
Joseph Ivie <joseph@lightningkite.com>
2023-02-02 17:28:16 -0700

    SFTP added as a file system

---
+++ server-sftp/build.gradle.kts
@@ -0,0 +1,67 @@
+import com.lightningkite.deployhelpers.developer
+import com.lightningkite.deployhelpers.github
+import com.lightningkite.deployhelpers.mit
+import com.lightningkite.deployhelpers.standardPublishing
+
+plugins {
+    kotlin("jvm")
+    id("com.google.devtools.ksp")
+    kotlin("plugin.serialization")
+    id("org.jetbrains.dokka")
+    id("signing")
+    `maven-publish`
+}
+
+val kotlinVersion: String by project
+val khrysalisVersion: String by project
+val coroutines:String by project
+dependencies {
+    api(project(":server-core"))
+    implementation("com.hierynomus:sshj:0.35.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutines")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation(project(":server-testing"))
+    ksp(project(":processor"))
+    kspTest(project(":processor"))
+}
+
+ksp {
+    arg("generateFields", "true")
+}
+
+kotlin {
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
+    sourceSets.test {
+        kotlin.srcDir("build/generated/ksp/test/kotlin")
+    }
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.freeCompilerArgs += "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"
+    kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+}
+
+
+standardPublishing {
+    name.set("Lightning-server-Server")
+    description.set("A set of tools to fill in/replace what Ktor is lacking in.")
+    github("lightningkite", "lightning-server")
+
+    licenses {
+        mit()
+    }
+
+    developers {
+        developer(
+            id = "LightningKiteJoseph",
+            name = "Joseph Ivie",
+            email = "joseph@lightningkite.com",
+        )
+        developer(
+            id = "bjsvedin",
+            name = "Brady Svedin",
+            email = "brady@lightningkite.com",
+        )
+    }
+}
