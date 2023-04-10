90efc39703d7f530c4dbdae85031ae5fb963d5ca
Brady Svedin <brady@lightningkite.com>
2023-04-10 17:52:47 -0600

    Added a way to handle errors in typescript across the entire sdk at once.

--- web-alt/src/apiCall.ts
+++ web-alt/src/apiCall.ts
@@ -10,7 +10,7 @@ type PathImpl<T, K extends keyof T> =
 
 export type Path<T> = PathImpl<T, keyof T> | (keyof T & string);
 
-export function apiCall<T>(url: string, body: T, request: RequestInit, fileUploads?: Record<Path<T>, File>): Promise<Response> {
+export function apiCall<T>(url: string, body: T, request: RequestInit, fileUploads?: Record<Path<T>, File>, errorHandlers?: Array<(x: Response)=>void> ): Promise<Response> {
     let f: Promise<Response>
     if(fileUploads === undefined || Object.keys(fileUploads).length === 0) {
         f = fetch(url, {
@@ -38,7 +38,12 @@ export function apiCall<T>(url: string, body: T, request: RequestInit, fileUploa
         })
     }
     return f.then(x => {
-        if(!x.ok) throw x
+        if(!x.ok) {
+            errorHandlers?.forEach(handler => {
+                handler(x)
+            })
+            throw x
+        }
         else return x
     })
 }
