15ff9ff428cac0c28bba36493e9bed57d6ca6d6d
Brady Svedin <brady@lightningkite.com>
2024-01-04 12:11:58 -0700

    Dependency version bumps and lambda runtime bump to 17.

--- server-dynamodb/build.gradle.kts
+++ server-dynamodb/build.gradle.kts
@@ -27,9 +27,9 @@ val khrysalisVersion: String by project
 dependencies {
     api(project(":server-core"))
 //    api(platform("software.amazon.awssdk:s3:2.17.232"))
-    api("software.amazon.awssdk:dynamodb:2.17.276")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.4")
+    api("software.amazon.awssdk:dynamodb:2.20.68")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.7.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation(project(":server-testing"))
     ksp(project(":processor"))
--- server-postgresql/build.gradle.kts
+++ server-postgresql/build.gradle.kts
@@ -23,16 +23,17 @@ repositories {
 
 val kotlinVersion: String by project
 val khrysalisVersion: String by project
+val exposedVersion = "0.39.2"
 dependencies {
     api(project(":server-core"))
-    api("org.jetbrains.exposed:exposed-core:0.39.2")
-    api("org.jetbrains.exposed:exposed-java-time:0.39.2")
-    api("org.jetbrains.exposed:exposed-jdbc:0.39.2")
+    api("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     api("org.postgresql:postgresql:42.5.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation(project(":server-testing"))
-    testImplementation("io.zonky.test:embedded-postgres:2.0.3")
+    testImplementation("io.zonky.test:embedded-postgres:2.0.4")
     ksp(project(":processor"))
     kspTest(project(":processor"))
 }
