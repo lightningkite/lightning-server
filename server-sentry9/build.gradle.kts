fc47d5a03800bd4557598c5264453dc1399cdb77
Joseph Ivie <joseph@lightningkite.com>
2024-02-01 17:01:41 -0700

    Sentry 9

---
+++ server-sentry9/build.gradle.kts
@@ -0,0 +1,73 @@
+import com.lightningkite.deployhelpers.developer
+import com.lightningkite.deployhelpers.github
+import com.lightningkite.deployhelpers.mit
+import com.lightningkite.deployhelpers.standardPublishing
+
+plugins {
+    kotlin("jvm")
+    id("com.google.devtools.ksp")
+    kotlin("plugin.serialization")
+    id("org.jetbrains.dokka")
+    id("signing")
+    `maven-publish`
+}
+
+val kotlinVersion: String by project
+val khrysalisVersion: String by project
+val coroutines: String by project
+dependencies {
+    api(project(":server-core"))
+    api("io.sentry:sentry:1.7.30")
+    api("io.sentry:sentry-logback:1.7.30")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutines")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    ksp(project(":processor"))
+    kspTest(project(":processor"))
+}
+
+ksp {
+    arg("generateFields", "true")
+}
+
+kotlin {
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
+    sourceSets.test {
+        kotlin.srcDir("build/generated/ksp/test/kotlin")
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    this.targetCompatibility = "11"
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.freeCompilerArgs += "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"
+    kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+}
+
+
+standardPublishing {
+    name.set("Lightning-server-Server")
+    description.set("An implementation of LightningServer ExceptionReporter using Sentry.")
+    github("lightningkite", "lightning-server")
+
+    licenses {
+        mit()
+    }
+
+    developers {
+        developer(
+            id = "LightningKiteJoseph",
+            name = "Joseph Ivie",
+            email = "joseph@lightningkite.com",
+        )
+        developer(
+            id = "bjsvedin",
+            name = "Brady Svedin",
+            email = "brady@lightningkite.com",
+        )
+    }
+}
+tasks.getByName("sourceJar").dependsOn("kspKotlin")
---
+++ server-sentry9/src/main/kotlin/com/lightningkite/lightningserver/exceptions/SentryExceptionReporter.kt
@@ -0,0 +1,146 @@
+package com.lightningkite.lightningserver.exceptions
+
+import com.lightningkite.lightningdb.HasEmail
+import com.lightningkite.lightningdb.HasId
+import com.lightningkite.lightningserver.auth.Authentication
+import com.lightningkite.lightningserver.auth.authAny
+import com.lightningkite.lightningserver.http.HttpRequest
+import com.lightningkite.lightningserver.schedule.Schedule
+import com.lightningkite.lightningserver.schedule.ScheduledTask
+import com.lightningkite.lightningserver.settings.generalSettings
+import com.lightningkite.lightningserver.tasks.Task
+import com.lightningkite.lightningserver.websocket.WebSockets
+import io.ktor.http.*
+import io.sentry.Sentry
+import io.sentry.SentryOptions
+import io.sentry.event.User
+import io.sentry.event.interfaces.HttpInterface
+
+/**
+ * An ExceptionReporter implementation that sends all reports to an external Sentry service.
+ *
+ * @param dsn The connection string used to connect to the Sentry Server.
+ */
+class SentryExceptionReporter(val dsn: String): ExceptionReporter {
+    companion object {
+        init {
+            ExceptionSettings.register("sentry") {
+                SentryExceptionReporter(it.url.substringAfter("://"))
+            }
+        }
+    }
+
+    init {
+        val name = generalSettings().projectName.filter { it.isLetterOrDigit() }
+        val version = System.getenv("AWS_LAMBDA_FUNCTION_VERSION")?.takeUnless { it.isBlank() } ?: "UNKNOWN"
+        Sentry.init("$dsn?release=$version&environment=$name")
+    }
+
+    override suspend fun report(t: Throwable, context: Any?): Boolean {
+        val ctx = Sentry.getContext()
+        when (context) {
+            is HttpRequest -> {
+                val p = context.authAny()?.get()
+                ctx.clear()
+                ctx.http = HttpInterface(
+                    context.endpoint.path.toString(),
+                    context.endpoint.method.toString(),
+                    context.queryParameters.groupBy { it.first }.mapValues { it.value.map { it.second } },
+                    null,
+                    context.headers.cookies.filter { it.key.lowercase() !in filterHeaders },
+                    context.sourceIp,
+                    null,
+                    -1,
+                    null,
+                    "null",
+                    -1,
+                    null,
+                    generalSettings().publicUrl.startsWith("https"),
+                    false,
+                    "",
+                    "",
+                    context.headers.normalizedEntries.filter { it.key.lowercase() !in filterHeaders },
+                    null
+                )
+                ctx.user = User(
+                    p.simpleUserId(),
+                    p.simpleUserTag(),
+                    context.sourceIp,
+                    p.simpleUserTag().takeIf { it.contains('@') },
+                    mapOf("stringRepresentation" to p.toString())
+                )
+                Sentry.capture(t)
+                ctx.clear()
+            }
+            is WebSockets.ConnectEvent -> {
+                ctx.clear()
+                val p = context.authAny()?.get()
+                ctx.http = HttpInterface(
+                    context.path.toString(),
+                    "WS",
+                    context.queryParameters.groupBy { it.first }.mapValues { it.value.map { it.second } },
+                    null,
+                    context.headers.cookies.filter { it.key.lowercase() !in filterHeaders },
+                    context.sourceIp,
+                    null,
+                    -1,
+                    null,
+                    "null",
+                    -1,
+                    null,
+                    generalSettings().publicUrl.startsWith("https"),
+                    false,
+                    "",
+                    "",
+                    context.headers.normalizedEntries.filter { it.key.lowercase() !in filterHeaders },
+                    null
+                )
+                ctx.user = User(
+                    p.simpleUserId(),
+                    p.simpleUserTag(),
+                    context.sourceIp,
+                    p.simpleUserTag().takeIf { it.contains('@') },
+                    mapOf("stringRepresentation" to p.toString())
+                )
+                Sentry.capture(t)
+                ctx.clear()
+            }
+            is Task<*> -> {
+                ctx.clear()
+                ctx.addTag("task", context.name)
+                Sentry.capture(t)
+                ctx.clear()
+            }
+            is ScheduledTask -> {
+                ctx.clear()
+                ctx.addTag("schedule", context.name)
+                Sentry.capture(t)
+                ctx.clear()
+            }
+            else -> {
+                ctx.clear()
+                ctx.addExtra("customContext", context.toString())
+                Sentry.capture(t)
+                ctx.clear()
+            }
+        }
+        return true
+    }
+}
+private val filterHeaders = setOf(
+    HttpHeaders.Authorization.lowercase(),
+    HttpHeaders.Cookie.lowercase(),
+)
+
+private fun Any?.simpleUserTag(): String = when(this) {
+    null -> "anonymous"
+    is HasEmail -> this.email
+    is HasId<*> -> this._id.toString()
+    else -> toString()
+}
+
+private fun Any?.simpleUserId(): String = when (this) {
+    null -> "anonymous"
+    is HasId<*> -> this._id.toString()
+    else -> toString()
+}
