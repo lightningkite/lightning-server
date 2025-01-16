3cec471c7e71ab34b7f1d9279794fdae9fe92b3c
Brady Svedin <brady@lightningkite.com>
2025-01-16 10:49:16 -0700

    Add timezone to schedule function

--- server-core/src/main/kotlin/com/lightningkite/lightningserver/schedule/dsl.kt
+++ server-core/src/main/kotlin/com/lightningkite/lightningserver/schedule/dsl.kt
@@ -2,6 +2,7 @@ package com.lightningkite.lightningserver.schedule
 
 import com.lightningkite.lightningserver.core.LightningServerDsl
 import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
 import kotlin.time.Duration
 
 @LightningServerDsl
@@ -12,15 +13,15 @@ fun schedule(name: String, frequency: Duration, action: suspend () -> Unit): Sch
 }
 
 @LightningServerDsl
-fun schedule(name: String, time: LocalTime, action: suspend () -> Unit): ScheduledTask {
-    val task = ScheduledTask(name, Schedule.Daily(time), action)
+fun schedule(name: String, time: LocalTime, timezone: TimeZone = TimeZone.currentSystemDefault(), action: suspend () -> Unit): ScheduledTask {
+    val task = ScheduledTask(name, Schedule.Daily(time, timezone), action)
     Scheduler.schedules[name] = task
     return task
 }
 
 @LightningServerDsl
-fun schedule(name: String, cron: CronPattern, action: suspend () -> Unit): ScheduledTask {
-    val task = ScheduledTask(name, Schedule.Cron(cron), action)
+fun schedule(name: String, cron: CronPattern, timezone: TimeZone = TimeZone.currentSystemDefault(), action: suspend () -> Unit): ScheduledTask {
+    val task = ScheduledTask(name, Schedule.Cron(cron, timezone), action)
     Scheduler.schedules[name] = task
     return task
 }
