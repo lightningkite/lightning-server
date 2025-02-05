4523d86cdfc01078f2145fb99b44ef9b62269402
Joseph Ivie <joseph@lightningkite.com>
2025-02-05 14:43:45 -0700

    tag testing for builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
         maven("https://lightningkite-maven.s3.us-west-2.amazonaws.com")
     }
     dependencies {
-        classpath("com.lightningkite:lk-gradle-helpers:1.0.2")
+        classpath("com.lightningkite:lk-gradle-helpers:1.0.3")
         classpath(libs.proguard)
     }
 }
