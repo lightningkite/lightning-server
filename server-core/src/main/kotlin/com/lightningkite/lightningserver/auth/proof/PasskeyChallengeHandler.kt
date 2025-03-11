7c946c6e3032bf45a46eb5ca04d55f7dad0fc2e0
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2025-03-11 16:12:09 -0600

    Lots of implementation work

--- server-core/src/main/kotlin/com/lightningkite/lightningserver/auth/proof/PasskeyChallengeHandler.kt
+++ server-core/src/main/kotlin/com/lightningkite/lightningserver/auth/proof/PasskeyChallengeHandler.kt
@@ -9,44 +9,60 @@ import java.util.*
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
-class ChallengeHandler(
+class PasskeyChallengeHandler(
     private val cache: () -> Cache,
     val keyPrefix: String,
-    val length: Int = 64,
+    val challengeLength: Int = 64,
     val expiration: Duration = 5.minutes
 ) {
-    private fun challengeCacheKey(challenge: String): String =
+
+    private fun registerChallengeCacheKey(challenge: String): String =
         "${keyPrefix}_passkey_register_challenge_${challenge.substring(0, 10)}"
     private fun subjectNameCacheKey(challenge: String): String =
         "${keyPrefix}_passkey_register_name_${challenge.substring(0, 10)}"
     private fun subjectIdCacheKey(challenge: String): String =
         "${keyPrefix}_passkey_register_id_${challenge.substring(0, 10)}"
 
-    suspend fun establish(subjectName: String, subjectId: String): String {
+    private fun loginChallengeCacheKey(challenge: String): String =
+        "${keyPrefix}_passkey_login_challenge_${challenge.substring(0, 10)}"
+
+    suspend fun establishForRegistration(subjectName: String, subjectId: String): String {
         val challenge = generate()
-        cache().set(challengeCacheKey(challenge), challenge, expiration)
+        cache().set(registerChallengeCacheKey(challenge), challenge, expiration)
         cache().set(subjectNameCacheKey(challenge), subjectName, expiration)
         cache().set(subjectIdCacheKey(challenge), subjectId, expiration)
         return challenge
     }
 
-    fun generate(): String {
-        val bytes = ByteArray(length)
+    suspend fun establishForLogin(): String {
+        val challenge = generate()
+        cache().set(loginChallengeCacheKey(challenge), challenge, expiration)
+        return challenge
+    }
+
+    private fun generate(): String {
+        val bytes = ByteArray(challengeLength)
         SecureRandom().nextBytes(bytes)
         return Base64.getEncoder().encodeToString(bytes)
     }
 
-    suspend fun assert(challenge: String): Pair<String, String> {
-        cache().get<String>(challengeCacheKey(challenge))
+    suspend fun assertForRegistration(challenge: String): Pair<String, String> {
+        cache().get<String>(registerChallengeCacheKey(challenge))
             ?.takeIf { it == challenge }
             ?: throw NotFoundException(detail = "unknown-challenge", message = "Challenge has expired.")
         val subjectName = cache().get<String>(subjectNameCacheKey(challenge))
             ?: throw IllegalStateException("No saved subject name")
         val subjectId = cache().get<String>(subjectIdCacheKey(challenge))
             ?: throw IllegalStateException("No saved subject id")
-        cache().remove(challengeCacheKey(challenge))
+        cache().remove(registerChallengeCacheKey(challenge))
         cache().remove(subjectNameCacheKey(challenge))
         cache().remove(subjectIdCacheKey(challenge))
         return subjectName to subjectId
+    }
+
+    suspend fun assertForLogin(challenge: String) {
+        cache().get<String>(loginChallengeCacheKey(challenge))
+            ?.takeIf { it == challenge }
+            ?: throw NotFoundException(detail = "unknown-challenge", message = "Challenge has expired.")
     }
 }
