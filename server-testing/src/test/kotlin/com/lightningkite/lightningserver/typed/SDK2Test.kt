a2306365d32e48dcc4a4e733da02e73231915d70
Joseph Ivie <joseph@lightningkite.com>
2025-03-28 15:27:35 -0600

    SDK2 in progress

--- server-testing/src/test/kotlin/com/lightningkite/lightningserver/typed/SDK2Test.kt
+++ server-testing/src/test/kotlin/com/lightningkite/lightningserver/typed/SDK2Test.kt
@@ -7,7 +7,15 @@ class SDK2Test {
     @Test fun test() {
         TestSettings
         with(SDK2) {
-            System.out.writeInterface("com.test")
+            System.out.writeInterface("com.lightningkite.lightningdb.test")
+            println("// -----------")
+            println("// -----------")
+            println("// -----------")
+            System.out.writeLive("com.lightningkite.lightningdb.test")
+            println("// -----------")
+            println("// -----------")
+            println("// -----------")
+            System.out.writeCached("com.lightningkite.lightningdb.test")
         }
     }
 }
