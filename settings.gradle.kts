2cefbc63929e1d8349bc01ad65b8bfe933f9683c
iHoonter <huntdawghero@gmail.com>
2025-06-19 14:16:24 -0600

    Added missing maven repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,7 @@ pluginManagement {
     dependencyResolutionManagement {
         repositories {
             mavenLocal()
+            maven("https://lightningkite-maven.s3.us-west-2.amazonaws.com")
             google()
             gradlePluginPortal()
             mavenCentral()
