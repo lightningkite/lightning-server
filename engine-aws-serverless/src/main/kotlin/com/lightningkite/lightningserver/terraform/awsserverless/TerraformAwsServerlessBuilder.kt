8d6aa3866809930214d6ab9ed3f7f489a8725812
Brady Svedin <brady@lightningkite.com>
2026-01-12 11:35:45 -0700

    Aws VPC module version update

--- engine-aws-serverless/src/main/kotlin/com/lightningkite/lightningserver/terraform/awsserverless/TerraformAwsServerlessBuilder.kt
+++ engine-aws-serverless/src/main/kotlin/com/lightningkite/lightningserver/terraform/awsserverless/TerraformAwsServerlessBuilder.kt
@@ -90,7 +90,6 @@ public abstract class TerraformAwsServerlessBuilder<S : ServerBuilder>(
                 null,
                 buildJsonObject { put("region", region.id()) })
         )
-        require(TerraformProvider(TerraformProviderImport.mongodbAtlas, null, JsonObject(emptyMap())))
         val emitter = this@TerraformAwsServerlessBuilder
 
         fulfillSetting(generalSettings.name, buildJsonObject {
