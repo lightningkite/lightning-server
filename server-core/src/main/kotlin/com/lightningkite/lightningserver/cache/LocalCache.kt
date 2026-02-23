f1d3d5bfda6f69f9049384cf6a0a4e43c0a96de8
Brady Svedin <brady@lightningkite.com>
2026-02-23 11:12:48 -0700

    LocalCache setIfNotExists, use compute for proper ttl checks and thread safety

--- server-core/src/main/kotlin/com/lightningkite/lightningserver/cache/LocalCache.kt
+++ server-core/src/main/kotlin/com/lightningkite/lightningserver/cache/LocalCache.kt
@@ -3,8 +3,8 @@ package com.lightningkite.lightningserver.cache
 import com.lightningkite.now
 import kotlinx.datetime.Instant
 import kotlinx.serialization.KSerializer
-import kotlin.time.Duration
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.time.Duration
 
 /**
  * A Cache implementation that exists entirely in the applications Heap. There are no external connections.
@@ -12,7 +12,7 @@ import java.util.concurrent.ConcurrentHashMap
  * This is useful in places that persistent data is not needed and speed is desired such as Unit Tests
  */
 open class LocalCache(val entries: ConcurrentHashMap<String, Entry> = ConcurrentHashMap()) : Cache {
-    companion object: LocalCache()
+    companion object : LocalCache()
     data class Entry(val value: Any?, val expires: Instant? = null)
 
     @Suppress("UNCHECKED_CAST")
@@ -31,13 +31,17 @@ open class LocalCache(val entries: ConcurrentHashMap<String, Entry> = Concurrent
         key: String,
         value: T,
         serializer: KSerializer<T>,
-        timeToLive: Duration?
+        timeToLive: Duration?,
     ): Boolean {
-        if (entries[key] == null) {
-            entries[key] = Entry(value, timeToLive?.let { now() + it })
-            return true
+        // We can't prove success using the response from the compute function, so we must manually handle it.
+        var success = false
+        entries.compute(key) { _, existing ->
+            if (existing == null || (existing.expires != null && existing.expires <= now())) {
+                success = true
+                Entry(value, timeToLive?.let { now() + it })
+            } else existing
         }
-        return false
+        return success
     }
 
     override suspend fun add(key: String, value: Int, timeToLive: Duration?) {
