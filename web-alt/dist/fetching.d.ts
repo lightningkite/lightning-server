f1f9048a21b5674396b66adb95a02c3b6656882f
Wesley Edwards <wesleyutahedwards@gmail.com>
2026-03-06 13:22:57 -0700

    remove web-alt. Migrated to separate repo.

--- web-alt/dist/fetching.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-export declare type HeaderCalculator = () => Promise<Record<string, string>> | Record<string, string>;
-export declare type Fetcher = <Body, T>(path: string, method: Method, body?: Body) => Promise<T>;
-export declare function createBasicFetcher(baseUrl: string, additionalHeaders?: HeaderCalculator, responseInterceptors?: (x: Response) => Response): Fetcher;
