f6649beb9238f0847d1537c5427488a14ad439e9
UnknownJoe796 <josephivie@gmail.com>
2026-06-26 11:58:35 -0600

    EC2 otel stuff, media type fix

--- files/src/test/kotlin/com/lightningkite/lightningserver/files/generated/LiveApi.kt
+++ files/src/test/kotlin/com/lightningkite/lightningserver/files/generated/LiveApi.kt
@@ -3,11 +3,10 @@ package com.lightningkite.lightningserver.files
 import com.lightningkite.lightningserver.HttpMethod
 import com.lightningkite.lightningserver.typed.Fetcher
 import kotlinx.serialization.ContextualSerializer
-import kotlinx.serialization.builtins.serializer
-import kotlinx.serialization.builtins.MapSerializer
-import kotlinx.serialization.builtins.ListSerializer
-import kotlinx.serialization.builtins.nullable
+import kotlinx.serialization.builtins.*
+import kotlinx.serialization.ExperimentalSerializationApi
 
+@OptIn(ExperimentalSerializationApi::class)
 class LiveApi(val fetcher: Fetcher) : Api {
 	override fun withHeaderCalculator(calculator: suspend () -> List<Pair<String, String>>): LiveApi = 
 		LiveApi(fetcher.withHeaderCalculator(calculator))
