e6c761984715abe17886ebe3626249d30eec7343
Brady Svedin <brady@lightningkite.com>
2026-08-18 13:44:24 -0600

    Fix ServerFileWithMetadata validators

--- media/src/main/kotlin/com/lightningkite/lightningserver/media/validation.kt
+++ media/src/main/kotlin/com/lightningkite/lightningserver/media/validation.kt
@@ -1,23 +1,42 @@
 package com.lightningkite.lightningserver.media
 
 import com.lightningkite.lightningserver.definition.Runtime
+import com.lightningkite.lightningserver.files.fileObject
+import com.lightningkite.services.data.DataSize.Companion.bytes
 import com.lightningkite.services.data.MediaType
 import com.lightningkite.services.data.MimeType
 import com.lightningkite.services.database.validation.AnnotationValidators
 
-private val mediaValidators = Runtime {
+public val AnnotationValidators.Companion.Media: Runtime<AnnotationValidators> get() = Media()
+
+public fun AnnotationValidators.Companion.Media(runSlowChecks: Boolean = true): Runtime<AnnotationValidators> = Runtime {
     AnnotationValidators {
-        validate<MimeType, ServerFileWithMetadata> { file ->
-            val acceptedTypes = types.map(::MediaType)
+        if (runSlowChecks) {
+            validateSuspending<MimeType, ServerFileWithMetadata> { file ->
+                val acceptedTypes = types.map(::MediaType)
+                val head = file.original.fileObject.head()
 
-            when {
-                file.size?.let { it > maxSize } == true -> "File is too big; max size is $maxSize bytes but file is ${file.size} bytes"
+                when {
+                    head == null -> "File does not exist"
+                    head.size > maxSize -> "File is too big; max size is $maxSize bytes but file is ${head.size.bytes} bytes"
+                    acceptedTypes.isNotEmpty() && acceptedTypes.none { it.accepts(head.type) }
+                        -> "File type ${file.mimeType} does not match any of $acceptedTypes"
+                    else -> null
+                }
+            }
+        } else {
+            validate<MimeType, ServerFileWithMetadata> { file ->
+                val acceptedTypes = types.map(::MediaType)
 
-                file.mimeType?.let { type ->
-                    acceptedTypes.none { it.accepts(type) }
-                } == true -> "File type ${file.mimeType} does not match any of $acceptedTypes"
+                when {
+                    file.size?.let { it > maxSize } == true -> "File is too big; max size is $maxSize bytes but file is ${file.size} bytes"
 
-                else -> null
+                    file.mimeType?.let { type ->
+                        acceptedTypes.none { it.accepts(type) }
+                    } == true -> "File type ${file.mimeType} does not match any of $acceptedTypes"
+
+                    else -> null
+                }
             }
         }
         validate<MimeType, ServerFileWithMetadataPreview> { preview ->
@@ -25,11 +44,9 @@ private val mediaValidators = Runtime {
 
             when {
                 preview.size > maxSize -> "File is too big; max size is $maxSize bytes but file is ${preview.size} bytes"
-                !preview.mimeType.accepts(preview.mimeType) -> "File type ${preview.mimeType} does not match any of $acceptedTypes"
+                acceptedTypes.isNotEmpty() && acceptedTypes.none { it.accepts(preview.mimeType) } -> "File type ${preview.mimeType} does not match any of $acceptedTypes"
                 else -> null
             }
         }
     }
 }
-
-public val AnnotationValidators.Companion.Media: Runtime<AnnotationValidators> get() = mediaValidators
